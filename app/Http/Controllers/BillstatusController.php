1c13301e31db2d491a9a52135bbc9748dedf63ad
LeeSilvestre <150642983+LeeSilvestre@users.noreply.github.com>
2024-05-25 00:17:27 +0800

    Update BillstatusController.php

--- app/Http/Controllers/BillstatusController.php
+++ app/Http/Controllers/BillstatusController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\BillStatus;
+use App\Models\BillDetails;
+use App\Models\Food;
 
 class BillstatusController extends Controller
 {
@@ -40,19 +42,38 @@ class BillstatusController extends Controller
 
     public function updateBillStatus($id)
     {
+        $bill = BillStatus::where('bill_id', $id)->first();
+
+        if ($bill && $bill->bill_status == 1) {
+            $billDetails = BillDetails::where('bill_id', $id)->get();
+            foreach ($billDetails as $detail) {
+                $food = Food::find($detail->food_id);
+                if ($food) {
+                    if ($food->food_qty >= $detail->item_qty) {
+                        $food->food_qty -= $detail->item_qty;
+                        $food->save();
+                    } else {
+                        return response()->json(['error' => 'Insufficient quantity for item ' . $detail->food_id], 400);
+                    }
+                } else {
+                    return response()->json(['error' => 'Food item not found for ID ' . $detail->food_id], 404);
+                }
+            }
+        }
+
         $billStatus = BillStatus::where('bill_id', $id)->increment('bill_status');
         return response()->json($billStatus);
     }
 
     public function updateBillPaid($id)
     {
-        $billStatus = BillStatus::where('bill_id', $id)->update(['bill_paid' => true]);
+        $billStatus = BillStatus::where('bill_id', $id)->update(['bill_paid' => 'true']);
         return response()->json($billStatus);
     }
 
     public function cancelBillStatus($id)
     {
-        $billStatus = BillStatus::where('bill_id', $id)->update(['bill_status' => 0, 'bill_paid' => false]);
+        $billStatus = BillStatus::where('bill_id', $id)->update(['bill_status' => 0, 'bill_paid' => 'false']);
         return response()->json($billStatus);
     }
 }
